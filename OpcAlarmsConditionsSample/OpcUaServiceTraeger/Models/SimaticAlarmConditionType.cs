2802edeb2a5b2455293caa5dc14afd42f3306bc7
Jan Biehl <jpbiehl@googlemail.com>
2023-04-27 11:56:08 +0200

    Updates

---
+++ OpcAlarmsConditionsSample/OpcUaServiceTraeger/Models/SimaticAlarmConditionType.cs
@@ -0,0 +1,34 @@
+﻿using Opc.UaFx;
+
+namespace OpcUaService.Models;
+
+[OpcEventType("ns=3;i=1805")]
+public class SimaticAlarmConditionType : OpcAlarmCondition
+{
+    public SimaticAlarmConditionType(IOpcReadOnlyNodeDataStore dataStore) : base(dataStore)
+    {
+    }
+    
+    private static readonly OpcName AssociatedValueName01 = new("3:AssociatedValue_01");
+	private static readonly OpcName AssociatedValueName02 = new("AssociatedValue_02", 3);
+	private static readonly OpcName AssociatedValueName03 = new("AssociatedValue_03", 3);
+	private static readonly OpcName AssociatedValueName04 = new("AssociatedValue_04", 3);
+	private static readonly OpcName AssociatedValueName05 = new("AssociatedValue_05", 3);
+	private static readonly OpcName AssociatedValueName06 = new("AssociatedValue_06", 3);
+	private static readonly OpcName AssociatedValueName07 = new("AssociatedValue_07", 3);
+	private static readonly OpcName AssociatedValueName08 = new("AssociatedValue_08", 3);
+	private static readonly OpcName AssociatedValueName09 = new("AssociatedValue_09", 3);
+	private static readonly OpcName AssociatedValueName10 = new("AssociatedValue_10", 3);
+	
+	public SimaticAssociatedAlarmValue AssociatedAlarmValue01 => DataStore.Get<SimaticAssociatedAlarmValue>(AssociatedValueName01);
+	public SimaticAssociatedAlarmValue AssociatedAlarmValue02 => DataStore.Get<SimaticAssociatedAlarmValue>(AssociatedValueName02);
+	public SimaticAssociatedAlarmValue AssociatedAlarmValue03 => DataStore.Get<SimaticAssociatedAlarmValue>(AssociatedValueName03);
+	public SimaticAssociatedAlarmValue AssociatedAlarmValue04 => DataStore.Get<SimaticAssociatedAlarmValue>(AssociatedValueName04);
+	public SimaticAssociatedAlarmValue AssociatedAlarmValue05 => DataStore.Get<SimaticAssociatedAlarmValue>(AssociatedValueName05);
+	public SimaticAssociatedAlarmValue AssociatedAlarmValue06 => DataStore.Get<SimaticAssociatedAlarmValue>(AssociatedValueName06);
+	public SimaticAssociatedAlarmValue AssociatedAlarmValue07 => DataStore.Get<SimaticAssociatedAlarmValue>(AssociatedValueName07);
+	public SimaticAssociatedAlarmValue AssociatedAlarmValue08 => DataStore.Get<SimaticAssociatedAlarmValue>(AssociatedValueName08);
+	public SimaticAssociatedAlarmValue AssociatedAlarmValue09 => DataStore.Get<SimaticAssociatedAlarmValue>(AssociatedValueName09);
+	public SimaticAssociatedAlarmValue AssociatedAlarmValue10 => DataStore.Get<SimaticAssociatedAlarmValue>(AssociatedValueName10);
+
+}
---
+++ OpcAlarmsConditionsSample/OpcUaServiceTraeger/OpcUaClientService.cs
@@ -0,0 +1,223 @@
+using dotenv.net.Utilities;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Opc.Ua;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+using OpcUaService.Models;
+
+namespace OpcUaService;
+
+public sealed class OpcUaClientService : IHostedService, IDisposable
+{
+	/// <summary>
+	/// Used to log messages
+	/// </summary>
+	private readonly ILogger<OpcUaClientService> _logger;
+	
+	/// <summary>
+	/// Subscriptions for alarms and events
+	/// </summary>
+	private OpcSubscription? _eventSubscriptions;
+
+	/// <summary>
+	/// Subscriptions for nodes
+	/// </summary>
+	private OpcSubscription? _nodeSubscriptions;
+
+	/// <summary>
+	/// Client that is connected to the OPC UA server
+	/// </summary>
+	private OpcClient? _opcClient;
+
+	/// <summary>
+	/// Flag to detect redundant calls
+	/// </summary>
+	private bool _disposed;
+	
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	/// <param name="logger">Logger that will be used for logging</param>
+	public OpcUaClientService(ILogger<OpcUaClientService> logger)
+	{
+		_logger = logger;
+	}
+
+	/// <inheritdoc />
+	public Task StartAsync(CancellationToken cancellationToken)
+	{
+		_logger.LogInformation("Opc service wird gestartet..");
+
+		_opcClient = new OpcClient(EnvReader.GetStringValue(EnvVars.EndpointUrlEnvVar), new OpcSecurityPolicy(OpcSecurityMode.None));
+		//_opcClient.UseDynamic = true;
+		
+		_opcClient.Connected += OpcClientOnConnected;
+		_opcClient.Disconnected += OpcClientOnDisconnected;
+		_opcClient.Reconnected += OpcClientOnReconnected;
+		
+		_opcClient.Connect();
+		
+		return Task.CompletedTask;
+	}
+
+	/// <inheritdoc />
+	public Task StopAsync(CancellationToken cancellationToken)
+	{
+		Dispose();
+		
+		return Task.CompletedTask;
+	}
+
+#region Callbacks
+	
+	private void OpcClientOnConnected(object? sender, EventArgs e)
+	{
+		_logger.LogInformation("Verbindung zum OPC UA Server wurde hergestellt.");
+		
+		OpcEvent.RegisterType<GenericEvent>("ns=3;i=1804");
+		
+		// Build the filter for the events
+		var filter = OpcFilter.Using(_opcClient)
+			.FromEvents(
+				OpcEventTypes.AlarmCondition,
+				OpcEventTypes.ExclusiveLimitAlarm,
+				OpcEventTypes.DialogCondition)
+			.Where(
+				OpcFilterOperand.OfType(OpcEventTypes.AlarmCondition) |
+				OpcFilterOperand.OfType(OpcEventTypes.AlarmCondition) |
+				OpcFilterOperand.OfType(OpcEventTypes.AlarmCondition)).Select();
+
+		// Subscribe at the server for events
+		if (OpcNodeId.TryParse(EnvReader.GetStringValue(EnvVars.AlarmsEventNodeEnvVar), out var nodeId))
+		{
+			if (!OpcNodeId.IsNullOrEmpty(nodeId))
+			{
+				//_eventSubscriptions = _opcClient?.SubscribeEvent(nodeId, filter, OnOpcEventReceived);
+				_eventSubscriptions = _opcClient?.SubscribeEvent(ObjectIds.Server, OnOpcEventReceived);
+
+				_eventSubscriptions?.RefreshConditions();
+				_eventSubscriptions?.ApplyChanges();
+
+				_logger.LogInformation("Event subscription wurde erstellt.");	
+			}
+			else
+			{
+				_logger.LogError("Es wurde keine gültige NodeId für die Events angegeben.");
+			}
+		}
+		else
+		{
+			_logger.LogError("Es wurde keine gültige NodeId für die Events angegeben.");
+		}
+	}
+	
+	private void OpcClientOnDisconnected(object? sender, EventArgs e)
+	{
+		_logger.LogInformation("Verbindung zum OPC UA Server wurde getrennt.");
+	}
+	
+	private void OpcClientOnReconnected(object? sender, EventArgs e)
+	{
+		_logger.LogInformation("Verbindung zum OPC UA Server wurde wiederhergestellt."); 
+	}
+	
+#endregion
+	
+	private void OnOpcEventReceived(object sender, OpcEventReceivedEventArgs e)
+	{
+		_logger.LogInformation("Event empfangen");
+		
+		var alarmEvent = e.Event;
+
+		switch (alarmEvent)
+		{
+			// case GenericEvent genericEvent:
+			// 	Print(genericEvent.GetData());
+			// 	break;
+			//
+			// case OpcAlarmCondition opcCondition:
+			// 	_logger.LogWarning("Nachricht: {Nachricht}", opcCondition.Message);
+			// 	_logger.LogWarning("Quittiert: {Quittiert}, Steht an: {StehtAn}", opcCondition.IsAcked, opcCondition.IsActive);
+			// 	break;
+			
+			// case OpcAlarmCondition opcCondition:
+			// 	_logger.LogWarning("Nachricht: {Nachricht}", opcCondition.Message);
+			// 	_logger.LogWarning("Quittiert: {Quittiert}, Steht an: {StehtAn}", opcCondition.IsAcked, opcCondition.IsActive);
+			// 	break;
+			
+			case SimaticAlarmConditionType simaticEvent:
+				_logger.LogWarning("Simatic event Nachricht: {Nachricht}", simaticEvent.Message);
+				break;
+		}
+	}
+	
+	private static void Print(Dictionary<string, object>? data, int level = 0)
+	{
+		if (data == null) 
+			throw new ArgumentNullException(nameof(data));
+		
+		var indent = new string('.', level * 2);
+
+		foreach (var item in data)
+		{
+			switch (item.Value)
+			{
+				case OpcDataObject instance:
+				{
+					Console.WriteLine(indent + item.Key);
+
+					var instanceData = instance.GetFields().ToDictionary(
+						field => field.Name,
+						field => field.Value);
+
+					Print(instanceData, level + 1);
+					break;
+				}
+				case OpcDataObject[] instances:
+				{
+					for (var index = 0; index < instances.Length; index++) 
+					{
+						Console.WriteLine(indent + "{0}[{1}]", item.Key, index);
+
+						var instanceData = instances[index].GetFields().ToDictionary(
+							field => field.Name,
+							field => field.Value);
+
+						Print(instanceData, level + 1);
+					}
+
+					break;
+				}
+				default:
+					Console.WriteLine(indent + "{0} = {1}", item.Key, item.Value);
+					break;
+			}
+		}
+	}
+	
+	/// <inheritdoc />
+	public void Dispose()
+	{
+		if (_disposed)
+			return;
+
+		_disposed = true;
+		
+		_eventSubscriptions?.Unsubscribe();
+		_eventSubscriptions = null;
+		
+		_nodeSubscriptions?.Unsubscribe();
+		_nodeSubscriptions = null;
+
+		if (_opcClient != null)
+		{
+			_opcClient.Connected -= OpcClientOnConnected;
+
+			_opcClient?.Disconnect();
+			_opcClient?.Dispose();
+		}
+
+		_opcClient = null;
+	}
+}
